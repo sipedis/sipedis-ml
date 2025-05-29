dbfdf05c69dcf058ee4332c925b653bf4884b18d
Arya Ulya Krisna <114469862+aryaulyakrisna@users.noreply.github.com>
2025-05-29 12:29:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const cors = require("cors);
+const cors = require("cors");
 
 
 // Inisialisasi Express
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/model", express.static("model"));
